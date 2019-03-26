9a376c8a13c7c9f3b9af4ff4274ff32268a921ba
Mark Agimba <markaguero16@gmail.com>
2019-03-26 16:17:14 +0300

    create upvote and downvote

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote'
 
 @Component({
@@ -8,12 +8,24 @@ import { Quote } from '../quote'
 })
 export class QuoteDetailsComponent implements OnInit {
 
-  @Input() quote:Quote;
-  @Output() isComplete= new EventEmitter<boolean>();
+  @Input() quote: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
 
-  quoteComplete(complete:boolean){
+  quoteDelete(complete: boolean) {
     this.isComplete.emit(complete);
   }
+
+  downvote = 0;
+  upvote = 0
+
+  voteDown() {
+    this.downvote = this.downvote + 1;
+  }
+
+  voteUp() {
+    this.upvote = this.upvote + 1;
+  }
+
   constructor() { }
 
   ngOnInit() {
