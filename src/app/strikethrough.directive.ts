c88e64f99531fcc0e4651e305d84412fe38f6324
Mark Agimba <markaguero16@gmail.com>
2019-03-26 15:39:58 +0300

    strikethrough ts

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,9 +1,11 @@
-import { Directive,ElementRef } from '@angular/core';
+import { Directive,ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
-  constructor(private elem:ElementRef){}
 
+  constructor(private elem:ElementRef){
+    this.elem.nativeElement.style.textDecoration='line-through';
+  }
 }
