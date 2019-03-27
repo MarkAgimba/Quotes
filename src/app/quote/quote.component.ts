787b3209035a52270e318cb5a27311bdf839630c
Mark Agimba <markaguero16@gmail.com>
2019-03-27 12:38:22 +0300

    form name

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   Quotes = [
-    new Quote(1, '-Martin Fowler','"Any fool can write code that a computer can understand. Good programmers write code that humans can understand."', new Date(2019, 2, 15)),
-    new Quote(2, '-Edsger Dijkstra', '“If debugging is the process of removing software bugs, then programming must be the process of putting them in”', new Date(2019, 3, 21)),
-    new Quote(3, '-Dan Salomon', '“Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.”', new Date(2019, 3, 20)),
-    new Quote(4, '-Kent Beck', '“Optimism is an occupational hazard of programming: feedback is the treatment.”', new Date(2019, 3, 23)),
-    new Quote(5, '-Eleanor Roosevelt', '"The future belongs to those who believe in the beauty of their dreams."', new Date(2019, 3, 24)),
-    new Quote(6, '-Bill Gates', '“Measuring programming progress by lines of code is like measuring aircraft building progress by weight.”', new Date(2019, 3, 25)),
+    new Quote(1, '-By Martin Fowler','"Any fool can write code that a computer can understand. Good programmers write code that humans can understand."', new Date(2019, 2, 15)),
+    new Quote(2, '-By Edsger Dijkstra', '“If debugging is the process of removing software bugs, then programming must be the process of putting them in”', new Date(2019, 3, 21)),
+    new Quote(3, '-By Dan Salomon', '“Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.”', new Date(2019, 3, 20)),
+    new Quote(4, '-By Kent Beck', '“Optimism is an occupational hazard of programming: feedback is the treatment.”', new Date(2019, 3, 23)),
+    new Quote(5, '-By Eleanor Roosevelt', '"The future belongs to those who believe in the beauty of their dreams."', new Date(2019, 3, 24)),
+    new Quote(6, '-By Bill Gates', '“Measuring programming progress by lines of code is like measuring aircraft building progress by weight.”', new Date(2019, 3, 25)),
   ]
 
   toggleDetails(index) {
